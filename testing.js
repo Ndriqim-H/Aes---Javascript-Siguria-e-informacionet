59e2452593c2c9f301ad43bb76fc7996904879eb
Ndriçim_Halili <ndriqim_@hotmail.com>
2022-11-30 15:05:10 +0100

    first commit

---
+++ testing.js
@@ -0,0 +1,10 @@
+'use strict'
+if (typeof module!='undefined' && module.exports) var Aes = require('./AES implementation in JavaScript'); // CommonJS (Node.js)
+if (typeof module!='undefined' && module.exports) Aes.Ctr = require('./AES Counter-mode implementation in JavaScript'); // CommonJS (Node.js)
+
+let x = Aes.Ctr.encrypt('big secret', 'password', 256);
+console.log('Aes.Ctr.encrypt: ' + x.base64Decode()); // encr: 'lwGl66VVwVOb
+
+let y = Aes.Ctr.decrypt('lwGl66VVwVObKIr6of8HVqJr', 'pāşšŵōřđ', 256);
+
+console.log('Aes.Ctr.decrypt: ' + y); 
